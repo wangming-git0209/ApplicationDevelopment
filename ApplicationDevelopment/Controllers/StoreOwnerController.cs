37602491783afabe404a8f803a9b55202c054167
Minh <minhnnqgcd191322@fpt.edu.vn>
2022-08-18 16:04:20 +0700

    Add Search feature

--- ApplicationDevelopment/Controllers/StoreOwnerController.cs
+++ ApplicationDevelopment/Controllers/StoreOwnerController.cs
@@ -3,6 +3,7 @@ using ApplicationDevelopment.Models;
 using ApplicationDevelopment.Utils;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace ApplicationDevelopment.Controllers
@@ -18,18 +19,67 @@ namespace ApplicationDevelopment.Controllers
             _userManager = userManager;
         }
 
-
-        public async Task<IActionResult> IndexAsync()
+        [HttpGet]
+        public async Task<IActionResult> IndexAsync(List<ApplicationUser> listCustomer)
         {
+            if (listCustomer.Count != 0)
+            {
+                return View(listCustomer);
+            }
             var usersInRole = await _userManager.GetUsersInRoleAsync(Role.CUSTOMER);
-             
             return View(usersInRole);
-        }       
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> IndexAsync(string searchString)
+        {
+
+            if (searchString == null)
+            {
+                return NotFound();
+            }
+            return await SearchCustomerAsync(searchString);
+
+            
+        }
+
+        public async Task<IActionResult> SearchCustomerAsync(string searchString)
+        {
+            List<ApplicationUser> listCustomer = new List<ApplicationUser>(); 
+            if (searchString == null)
+            {
+                return NotFound();
+            }
+            var customer = await _userManager.FindByEmailAsync(searchString);
+            listCustomer.Add(customer); 
+
+            return View("Index", listCustomer); 
+        }
 
 
+        [HttpGet]          
         public IActionResult SendCategoryRequest()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult SendCategoryRequest(Category category)
+        {
+            if(category == null)
+            {
+                return BadRequest();
+            }
+
+            Category newCategory = new Category();
+
+            newCategory.NameCategory = category.NameCategory;
+            newCategory.Status = Enums.CategoryStatus.InProgess;
+
+            _context.Add(newCategory);
+            _context.SaveChanges();
+
+            return View(newCategory);
         }
     }
 }
